53dbb24f25e3deef9ec587b453b84e5721a44f02
“[natali <[natali.velandia@gmail.com]>
2025-05-14 09:46:01 -0500

    Visualizacion cambiada

--- visualizacion.py
+++ visualizacion.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 from analisis import DataAnalyzer
+import tkinter as tk
+from tkinter import filedialog, messagebox, simpledialog
+from tkinter.scrolledtext import ScrolledText
 
 data = pd.read_csv("adult.csv")
 analizar = DataAnalyzer(data)
@@ -19,3 +22,24 @@ analizar = DataAnalyzer(data)
 analizar.summary()
 analizar.correlation_matrix()
 analizar.categorical_analisis()
+
+
+
+# --- Interfaz gráfica ---
+root = tk.Tk()
+root.title("Análisis de Datos con Pandas")
+
+btn_load = tk.Button(root, text="Cargar CSV")
+btn_load.pack(pady=10)
+
+text_area = ScrolledText(root, width=100, height=30)
+text_area.pack()
+
+btns_frame = tk.Frame(root)
+btns_frame.pack(pady=10)
+
+tk.Button(btns_frame, text="Resumen Básico").grid(row=0, column=0, padx=5)
+tk.Button(btns_frame, text="Análisis Categórico").grid(row=0, column=1, padx=5)
+tk.Button(btns_frame, text="Análisis Numérico").grid(row=0, column=2, padx=5)
+
+root.mainloop()
